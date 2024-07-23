c203ee2bfd785f2e46ee9564f63c96375e8823fe
Demian Tavolaro <demiantavolaro6@gmail.com>
2024-07-22 22:02:41 -0300

    Exercises 5.17 to 5.21

--- tests/bloglist-test.spec.js
+++ tests/bloglist-test.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Blog App', () => {
   test.beforeEach(async ({ request, page }) => {
-    await request.post('http://localhost:3003/api/testing/rest');
+    await request.post('http://localhost:3003/api/testing/reset');
 
     await request.post('http://localhost:3003/api/users', {
       data: {
@@ -32,6 +32,59 @@ test.describe('Blog App', () => {
       await page.getByRole('textbox').last().fill('1234Pepo56');
       await page.getByRole('button', { name: 'Login' }).click();
       await expect(page.getByText('wrong')).toBeVisible();
+    });
+
+    test.describe('When logged in', () => {
+      test.beforeEach(async ({ page, request }) => {
+        await page.getByRole('textbox').first().fill('PruebaJorge');
+        await page.getByRole('textbox').last().fill('1234Pepo');
+        await page.getByRole('button', { name: 'Login' }).click();
+      });
+
+      test('a new blog can be created', async ({ page, request }) => {
+        await page.getByRole('button', { name: 'New blog' }).click();
+
+        await page.getByLabel('title').fill('Prueba play');
+        await page.getByLabel('author').fill('firulai');
+        await page.getByLabel('url').fill('www.google.com');
+
+        await page.getByRole('button', { name: 'Save' }).click();
+
+        await expect(page.getByText('Prueba play')).toBeVisible();
+      });
+
+      test('the blog can be liked', async ({ page, request }) => {
+        await page.getByRole('button', { name: 'New blog' }).click();
+
+        await page.getByLabel('title').fill('Prueba play');
+        await page.getByLabel('author').fill('firulai');
+        await page.getByLabel('url').fill('www.google.com');
+
+        await page.getByRole('button', { name: 'Save' }).click();
+        await page.getByRole('button', { name: 'Show' }).click();
+
+        await page.getByRole('button', { name: 'Like' }).click();
+
+        await expect(page.getByText('1')).toBeVisible();
+      });
+
+      test('a blog can be deleted', async ({ page, request }) => {
+        await page.getByRole('button', { name: 'New blog' }).click();
+
+        await page.getByLabel('title').fill('Prueba play');
+        await page.getByLabel('author').fill('firulai');
+        await page.getByLabel('url').fill('www.google.com');
+
+        await page.getByRole('button', { name: 'Save' }).click();
+        await page.getByRole('button', { name: 'Show' }).click();
+        await page.getByRole('button', { name: 'Delete' }).click();
+
+        page.on('dialog', async (dialog) => {
+          expect(dialog.message()).toContain('Pipoc');
+
+          await dialog.accept();
+        });
+      });
     });
   });
 });
